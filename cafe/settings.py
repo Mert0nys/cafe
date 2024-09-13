432bb368845d117a1548bbd627a1f7c20c78abe1
Mert0nys <168013436+Mert0nys@users.noreply.github.com>
2024-09-14 01:59:48 +0400

    15

--- cafe/settings.py
+++ cafe/settings.py
@@ -66,8 +66,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-       "http://127.0.0.1:8000",
-       "http://localhost:8000",
+
        "https://mert0nys-cafe-c2cd.twc1.net"
 
    ]
@@ -76,7 +75,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://mert0nys-cafe-e5e5.twc1.net',
+    'https://mert0nys-cafe-c2cd.twc1.net',
 ]
 
 
